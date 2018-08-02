ff82eeb8a4a25c44f32f1750200ac6686790d3f3
kurokyfan <kuro2007cumt@gmail.com>
2018-08-02 17:37:02 +0800

    update

--- MXVideo/MXVideo/MainViewController.swift
+++ MXVideo/MXVideo/MainViewController.swift
@@ -12,16 +12,12 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        
+        self.view.backgroundColor = UIColor.white
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
